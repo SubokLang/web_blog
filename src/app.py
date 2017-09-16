5d14168dad6142f5abbd126b66c7a6c5d5ce113b
suboklang <github224427>
2017-09-15 19:21:33 -0700

    013 - User Registration Page

--- src/app.py
+++ src/app.py
@@ -6,15 +6,22 @@ app = Flask(__name__)
 app.secret_key = "123"
 
 @app.route('/')
-def hello_method():
+def home_template():
+    return render_template('home.html')
+
+@app.route('/login')
+def login_template():
     return render_template('login.html')
+
+@app.route('/register')
+def register_template():
+    return render_template('register.html')
 
 @app.before_first_request
 def initialize_database():
     Database.initialize()
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
@@ -23,6 +30,15 @@ def login_user():
         User.login(email)
     else:
         session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email,password)
 
     return render_template("profile.html", email=session['email'])
 
